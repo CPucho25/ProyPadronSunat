2bcf8e8302f87a65761084fda3375ec6f9a8edfa
Carlos Pucho Huamani <Carlos.Pucho@pe.ey.com>
2021-06-15 11:40:44 -0500

    Add project files.

---
+++ ProyPadronSunat/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ProyPadronSunat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.BtnEliminarFiles = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Inicio :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Fin :";
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.Location = new System.Drawing.Point(96, 57);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(176, 44);
+            this.btnIniciar.TabIndex = 5;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(96, 231);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(176, 39);
+            this.btnSalir.TabIndex = 6;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // BtnEliminarFiles
+            // 
+            this.BtnEliminarFiles.AutoSize = true;
+            this.BtnEliminarFiles.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnEliminarFiles.BackgroundImage")));
+            this.BtnEliminarFiles.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnEliminarFiles.FlatAppearance.BorderSize = 0;
+            this.BtnEliminarFiles.Location = new System.Drawing.Point(152, 295);
+            this.BtnEliminarFiles.Name = "BtnEliminarFiles";
+            this.BtnEliminarFiles.Size = new System.Drawing.Size(64, 64);
+            this.BtnEliminarFiles.TabIndex = 7;
+            this.BtnEliminarFiles.UseVisualStyleBackColor = true;
+            this.BtnEliminarFiles.Click += new System.EventHandler(this.BtnEliminarFiles_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 382);
+            this.Controls.Add(this.BtnEliminarFiles);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button BtnEliminarFiles;
+    }
+}
+
---
+++ ProyPadronSunat/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ProyPadronSunat
+{
+    public partial class Form1 : Form
+    {
+        private static IWebDriver driver;
+
+        //Ruta de Descarga
+        static string path1_DWL = Environment.CurrentDirectory + @"\1-Downloads\";
+        static string path2_Unzip = Environment.CurrentDirectory + @"\2-Unzip\";
+        static string path3_txtFormat = Environment.CurrentDirectory + @"\3-TxtFormat\";
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            createPaths();
+
+            //option por default para descarga multiple
+            var options = new ChromeOptions();
+            options.AddUserProfilePreference("profile.default_content_setting_values.automatic_downloads", 1);
+            options.AddUserProfilePreference("download.default_directory", path1_DWL);
+
+            driver = new ChromeDriver(options);
+
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString();
+
+            string url = "https://www.sunat.gob.pe/descargaPRR/mrc137_padron_reducido.html";
+
+            driver.Url = url;
+
+            #region Descarga txt web sunat
+
+            Thread.Sleep(2000);
+
+            IWebElement iDownloadZip = driver.FindElement(By.XPath("/html/body/div[2]/div[1]/div[2]/a"));
+            iDownloadZip.Click();
+
+            #endregion
+
+            Thread.Sleep(30000);
+
+            //Ruta donde se descomprimira los archivos ZIP
+
+            #region Descomprimir ZIP
+
+            string pathUnzip = Environment.CurrentDirectory + @"\2-Unzip\";
+
+            if (!Directory.Exists(pathUnzip))
+            { Directory.CreateDirectory(pathUnzip); }
+
+            bool existeFile = false;
+
+            while (existeFile == false)
+            {
+                DirectoryInfo di1 = new DirectoryInfo(path1_DWL);
+                string name1;
+
+                foreach (var fi1 in di1.GetFiles())
+                {
+                    name1 = fi1.Name.Substring(0, 6);
+
+                    if (name1.Equals("padron"))
+                    {
+                        ZipFile.ExtractToDirectory(path1_DWL + fi1.Name, pathUnzip);
+
+                        existeFile = true;
+                    }
+                    else
+                    {
+                        Thread.Sleep(10000);
+
+                        existeFile = false;
+                    }
+                }
+            }
+
+            #endregion
+
+            Thread.Sleep(5000);
+
+            #region Formatear TXT
+
+            DirectoryInfo di2 = new DirectoryInfo(path2_Unzip);
+
+            foreach (var fi2 in di2.GetFiles())
+            {
+                reemplazarCaracter(fi2.Name);
+            }
+
+            #endregion
+
+            Thread.Sleep(3000);
+
+            cleanFiles("");
+
+            label2.Text = DateTime.Now.ToString();
+        }
+
+        public void reemplazarCaracter(string nameFile) {
+
+            string nameFile2 = nameFile.Substring(0, nameFile.Length - 4);
+
+            string path1 = path2_Unzip + nameFile;
+            string path2 = path3_txtFormat + nameFile2 + "_Res.txt";
+            //string path3 = Environment.CurrentDirectory + @"\padron_reducido_ruc_Res_1.txt";
+
+            string[] arr = File.ReadAllLines(path1, Encoding.Default);
+            int large = arr.Length;
+
+            string line;
+
+            var stopwatch = new Stopwatch(); // 2) Nueva Linea
+
+            stopwatch.Start();
+
+            using (StreamWriter streamwriter1 = File.AppendText(path2))
+            {
+                for (int i = 0; i < large; i++)
+                {
+                    line = arr[i];
+                    line = line.Replace("||", "|").Replace(@"\|", "").Replace(@"\", ".");
+
+                    streamwriter1.WriteLine(line.Substring(0, line.Length - 1));
+                }
+            }
+            stopwatch.Stop();
+
+        }
+
+        public void createPaths() {
+
+            string path1 = Environment.CurrentDirectory + @"\1-Downloads\";
+            string path2 = Environment.CurrentDirectory + @"\2-Unzip\";
+            string path3 = Environment.CurrentDirectory + @"\3-TxtFormat\";
+
+            if (!Directory.Exists(path1))
+            {
+                Directory.CreateDirectory(path1);
+            }
+            if (!Directory.Exists(path2))
+            {
+                Directory.CreateDirectory(path2);
+            }
+            if (!Directory.Exists(path3))
+            {
+                Directory.CreateDirectory(path3);
+            }
+        }
+
+        public void cleanFiles(string all)
+        {
+
+            DirectoryInfo di11 = new DirectoryInfo(path1_DWL);
+
+            foreach (var fi11 in di11.GetFiles())
+            {
+                File.Delete(path1_DWL + fi11.Name);
+            }
+
+            DirectoryInfo di12 = new DirectoryInfo(path2_Unzip);
+
+            foreach (var fi12 in di12.GetFiles())
+            {
+                File.Delete(path2_Unzip + fi12.Name);
+            }
+
+            if (all.Equals("all"))
+            {
+
+                DirectoryInfo di13 = new DirectoryInfo(path3_txtFormat);
+
+                foreach (var fi13 in di13.GetFiles())
+                {
+                    File.Delete(path3_txtFormat + fi13.Name);
+                }
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            driver.Close();
+            this.Close();
+        }
+
+        private void BtnEliminarFiles_Click(object sender, EventArgs e)
+        {
+            cleanFiles("all");
+
+            MessageBox.Show("Archivos eliminados correctamente");
+        }
+    }
+}
